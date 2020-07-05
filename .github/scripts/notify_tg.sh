9dbccebe238606f031fdb7bfee20cda14735155c
Vasilii Emelin <vemelin.c@d2iq.com>
2020-07-05 15:55:43 +0300

    vars
    Signed-off-by: Vasilii Emelin <vemelin.c@d2iq.com>

--- .github/scripts/notify_tg.sh
+++ .github/scripts/notify_tg.sh
@@ -2,3 +2,7 @@
 
 curl -X POST https://api.telegram.org/bot576734993:AAFhJQALvnwzXR8ZWlLGkRaZePlUcGGwMmA/sendMessage -d chat_id=-1001495505532 \
 -d "text=dummy experimental bot message"
+
+echo trying to output env vars:
+echo repo is ${github.repository}
+echo actor is ${ACTOR}

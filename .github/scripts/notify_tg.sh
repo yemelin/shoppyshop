f595d8ada3317a67d4eea58133f9158654f19af1
Vasilii Emelin <yemelin@gmail.com>
2020-07-05 20:58:06 +0300

    PR handling
    Signed-off-by: Vasilii Emelin <yemelin@gmail.com>

--- .github/scripts/notify_tg.sh
+++ .github/scripts/notify_tg.sh
@@ -3,6 +3,13 @@
 #curl -X POST https://api.telegram.org/bot576734993:AAFhJQALvnwzXR8ZWlLGkRaZePlUcGGwMmA/sendMessage -d chat_id=-1001495505532 \
 #-d "text=${GITHUB_ACTOR} successfully pushed branch ${GITHUB_REF##*/}"
 BRANCH_NAME=${GITHUB_REF##*/}
+echo "${GITHUB_EVENT_NAME}"
+
+if [ "${GITHUB_EVENT_NAME}" != "push" ]; then
+  curl -X POST https://api.telegram.org/bot${BOT_ID}/sendMessage -d chat_id=${CHAT_ID} \
+  -d "text=something PR-related happened"
+  exit 0
+fi
 
 echo "text=\`${GITHUB_ACTOR}\` *successfully* __pushed__ branch [${BRANCH_NAME//-/\\-}](${GITHUB_REPOSITORY}/tree/${BRANCH_NAME//-/\\-})"
 curl -X POST https://api.telegram.org/bot${BOT_ID}/sendMessage -d chat_id=${CHAT_ID} \

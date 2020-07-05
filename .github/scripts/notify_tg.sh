b1ee9160671b1aad0492af59434aa6580e9b3d5a
Vasilii Emelin <yemelin@gmail.com>
2020-07-05 16:31:49 +0300

    message and secrets
    Signed-off-by: Vasilii Emelin <yemelin@gmail.com>

--- .github/scripts/notify_tg.sh
+++ .github/scripts/notify_tg.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-curl -X POST https://api.telegram.org/bot576734993:AAFhJQALvnwzXR8ZWlLGkRaZePlUcGGwMmA/sendMessage -d chat_id=-1001495505532 \
--d "text=dummy experimental bot message"
+#curl -X POST https://api.telegram.org/bot576734993:AAFhJQALvnwzXR8ZWlLGkRaZePlUcGGwMmA/sendMessage -d chat_id=-1001495505532 \
+#-d "text=${GITHUB_ACTOR} successfully pushed branch ${GITHUB_REF##*/}"
+curl -X POST https://api.telegram.org/bot${BOT_ID}/sendMessage -d chat_id=${CHAT_ID} \
+-d "text=${GITHUB_ACTOR} successfully pushed branch ${GITHUB_REF##*/}"
 
 echo trying to output env vars:
-
-echo actor is ${ACTOR}
+echo actor is ${GITHUB_ACTOR}
 echo branch is ${GITHUB_REF##*/}

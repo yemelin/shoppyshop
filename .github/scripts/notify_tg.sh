7c4ed2467d284d84e9ca672c050dbc6eea9aa012
Vasilii Emelin <vemelin.c@d2iq.com>
2020-07-05 15:24:34 +0300

    try to add bot
    Signed-off-by: Vasilii Emelin <vemelin.c@d2iq.com>

---
+++ .github/scripts/notify_tg.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+curl -X POST https://api.telegram.org/bot576734993:AAFhJQALvnwzXR8ZWlLGkRaZePlUcGGwMmA/sendMessage -d chat_id=-1001495505532 \
+-d "text=dummy experimental bot message"

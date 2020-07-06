91f46684bdec2675db5e84bad363dd17daa97786
Vasilii Emelin <yemelin@gmail.com>
2020-07-06 13:39:39 +0300

    disable web link preview
    Signed-off-by: Vasilii Emelin <yemelin@gmail.com>

--- .github/scripts/notify_tg.sh
+++ .github/scripts/notify_tg.sh
@@ -13,7 +13,7 @@ fi
 
 echo "text=\`${GITHUB_ACTOR}\` *successfully* __pushed__ branch [${BRANCH_NAME//-/\\-}](${GITHUB_REPOSITORY}/tree/${BRANCH_NAME//-/\\-})"
 curl -X POST https://api.telegram.org/bot${BOT_ID}/sendMessage -d chat_id=${CHAT_ID} \
--d "text=\`${GITHUB_ACTOR}\` *successfully* __pushed__ branch [${BRANCH_NAME//-/\\-}](https://github.com/${GITHUB_REPOSITORY}/tree/${BRANCH_NAME//-/\\-})" -d parse_mode=MarkdownV2
+-d "text=\`${GITHUB_ACTOR}\` *successfully* __pushed__ branch [${BRANCH_NAME//-/\\-}](https://github.com/${GITHUB_REPOSITORY}/tree/${BRANCH_NAME//-/\\-})" -d parse_mode=MarkdownV2 -d disable_web_page_preview=true
 
 #echo trying to output env vars:
 #echo actor is ${GITHUB_ACTOR}

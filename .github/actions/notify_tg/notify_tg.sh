0b854df0562e11db73ba4b91fb0c649cd17e6c05
Vasilii Emelin <yemelin@gmail.com>
2020-07-12 01:55:42 +0300

    build failure notify
    Signed-off-by: Vasilii Emelin <yemelin@gmail.com>

--- .github/actions/notify_tg/notify_tg.sh
+++ .github/actions/notify_tg/notify_tg.sh
@@ -1,46 +1,28 @@
 #! /bin/sh
 
+set -eu
+
 # add to secrets and remove
-#BOT_ID=576734993:AAFhJQALvnwzXR8ZWlLGkRaZePlUcGGwMmA
-#CHAT_ID=-1001174776301
+BOT_ID=576734993:AAFhJQALvnwzXR8ZWlLGkRaZePlUcGGwMmA
+CHAT_ID=-1001174776301
 
-BRANCH_NAME=${GITHUB_REF##*/}
-
-echo trying to output vars
-echo "MESSAGE: ${MSG}"
-echo "BRANCH: ${BRANCH_NAME}"
-echo "PR_LINK: ${PR_LINK}"
-echo "GITHUB_EVENT_NAME: ${GITHUB_EVENT_NAME}"
-echo "GITHUB_ACTOR ${GITHUB_ACTOR}"
-
-echo "Command-line vars:"
-echo "$1"
-echo "$2 $3"
-
+#BRANCH_NAME=${GITHUB_REF##*/}
 
 MSG=$1
-BOT_ID=$2
-CHAT_ID=$3
-
-
+#BOT_ID=$2
+#CHAT_ID=$3
 
 # this script is currently used for PR notifications only, so we keep it simple while we can
 # once push notification is needed, branch extraction below will come in handy
 # "branch: [${BRANCH_NAME}](https://github.com/${GITHUB_REPOSITORY}/tree/${BRANCH_NAME})"
 
-#read -r -d '' MSG <<- EOM
-#*event*: ${GITHUB_EVENT_NAME}
-#*actor*: ${GITHUB_ACTOR}
-#*action*: ${ACTION}
-#"*PR*: [${PR_TITLE}](${PR_LINK})"
-#EOM
-
 # escaping _ and - for Telegram to parse Markdown correctly
 MSG=${MSG//-/\\-}
 MSG=${MSG//_/\\_}
 
+echo "sending message: ${MSG}"
 
-curl -X POST https://api.telegram.org/bot${BOT_ID}/sendMessage -d chat_id="${CHAT_ID}" \
+curl -X POST https://api.telegram.org/bot"${BOT_ID}"/sendMessage -d chat_id="${CHAT_ID}" \
 -d "text=${MSG}" -d parse_mode=MarkdownV2 -d disable_web_page_preview=true
 
 
